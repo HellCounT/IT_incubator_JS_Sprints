a7a5df6166a6087d56fce98a1d304e865f5bb61c
HellCounT <a_killaheadz@live.ru>
2022-12-07 22:12:30 +0300

    upd

---
+++ Sprint 1 - 01/main.js
@@ -0,0 +1,121 @@
+//num, str, boolean, null, undefined, NaN
+//object, array, function
+
+const a = 4;
+const b = a;
+
+const user1 = {  //#567 - memory cell
+    name: "Bob",
+    age: "23",
+    isStudent: "true"
+}
+
+const user2 = user1; //#567 - same memory cell
+user2.name = "Ann";
+console.log(user1);
+
+//делаем копию = вносим изменения в копию = используем копию с изменениями
+
+/*const copyUser = {}; //new Object()
+copyUser.name = user1.name;
+copyUser.age = user1.age;
+copyUser.isStudent = user1.isStudent;*/
+
+const copyUser = {...user1};
+
+console.log(copyUser);
+console.log(user1 === copyUser); //false
+
+const copyUser1 = {...user1, isStudent: false} //изменение существующего свойства
+const copyUser2 = {...user1, isMarried: false} //добавление свойства
+
+const address = {
+    country: "Thailand",
+    city: "Bangkok"
+}
+
+const copyUser3 = {...user1, ...address}
+console.log(copyUser3)
+
+const student = {
+    name: "Bob",
+    age: "23",
+    isStudent: "true",
+    address: {  // #789
+        country: "Thailand",
+        city: "Bangkok"
+    },
+    courses: ["HTML", "CSS", "JS"] // #987
+}
+
+const copyStudent = {...student} // #789 % #987
+copyStudent.courses.push("React")
+console.log(student)
+
+const copyStudent2 = {...student, courses: [...student.courses, "Redux"]} // ... - spread operator
+
+console.log(student.courses)
+console.log(copyStudent2.courses)
+
+const users = [
+    {
+        id: 1,
+        name: "Bob",
+        isStudent: true,
+        address: {
+            country: "Sudan",
+            city: "Hartum",
+        }
+
+    },
+    {
+        id: 2,
+        name: "Alex",
+        isStudent: true,
+        address: {
+            country: "Sudan",
+            city: "Hartum",
+        }
+    },
+    {
+        id: 3,
+        name: "Ann",
+        isStudent: true,
+        address: {
+            country: "Sudan",
+            city: "Hartum",
+        }
+    },
+    {
+        id: 4,
+        name: "Donald",
+        isStudent: true,
+        address: {
+            country: "Sudan",
+            city: "Hartum",
+        }
+    },
+]
+
+const newUser = {
+    id: 5,
+    name: "Donald",
+    isStudent: true,
+    address: {
+        country: "Sudan",
+        city: "Hartum",
+    }
+}
+
+const users2 = [...users, newUser] // добавление эл-та в массив
+
+// delete id === 3
+
+const users3 = users2.filter(user => user.id !== 3) //false
+console.log(users3)
+
+// User id = 1 moved to Sudan city
+
+const users4 = users3.map(u => u.id === 1 ? {...u, address: {...u.address, city: "Sudan"}} : u)
+console.log(users4)
+const users5 = users4.map(u => u.id === 1 ? {...u, name: "Ali"} : u)
